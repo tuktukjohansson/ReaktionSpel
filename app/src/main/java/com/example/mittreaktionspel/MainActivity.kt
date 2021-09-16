85b755fed93c1a925082018b2f59621c5347b42a
tuktukjohansson <marcusjohansson0111@gmail.com>
2021-09-16 21:09:15 +0200

    +Instruktioner tillagda +Vissa knappars funktionalitet är kodade +Länkat en knapp till att öppna en ny aktivitet +Lagt till en del bilder för knapparna och instruktionerna på png filer.

--- app/src/main/java/com/example/mittreaktionspel/MainActivity.kt
+++ app/src/main/java/com/example/mittreaktionspel/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.mittreaktionspel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Variables
+        val buttonplay = findViewById<Button>(R.id.buttonp)
+        val buttonrules = findViewById<Button>(R.id.buttoni)
+        val instructionimage = findViewById<ImageView>(R.id.infoview)
+        val infocloser = findViewById<ImageButton>(R.id.infoclose1)
+        // Variables
+
+        // ClickEvents
+        buttonplay.setOnClickListener(){
+            val intent = Intent(this, MainGameActivity::class.java)
+            startActivity(intent)
+        }
+        buttonrules.setOnClickListener(){
+            instructionimage.setVisibility(View.VISIBLE)
+            buttonplay.setVisibility(View.GONE)
+            buttonrules.setVisibility(View.GONE)
+            infocloser.setVisibility(View.VISIBLE)
+        }
+        infocloser.setOnClickListener(){
+            instructionimage.setVisibility(View.GONE)
+            buttonplay.setVisibility(View.VISIBLE)
+            buttonrules.setVisibility(View.VISIBLE)
+            infocloser.setVisibility(View.GONE)
+        }
+        // ClickEvents
     }
 }
+
+
---
+++ app/src/main/java/com/example/mittreaktionspel/MainGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mittreaktionspel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_game)
+    }
+}
