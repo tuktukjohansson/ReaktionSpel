eb52479a2091cb65036793a31bb39fd03e00682a
tuktukjohansson <marcusjohansson0111@gmail.com>
2021-09-21 14:09:17 +0200

    +Lagt till 5 stycken funktioner i min andra aktivitet (@MainGameActivity) där två olika timers används som spelets grund byggs på. Först en slumpad timer när kortet visar sig innan andra timern går igång för svårighetsgraden, för varje gång programmet körs kommer den andra timern gå 10% snabbare

--- app/src/main/java/com/example/mittreaktionspel/MainGameActivity.kt
+++ app/src/main/java/com/example/mittreaktionspel/MainGameActivity.kt
@@ -2,10 +2,56 @@ package com.example.mittreaktionspel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.os.CountDownTimer
+import android.widget.TextView
+import android.widget.Toast
 
-class MainGameActivity : AppCompatActivity() {
+
+public class MainGameActivity : AppCompatActivity() {
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------\\
+    //Variabler\\
+    var kort = (0..4).random()
+    var milishowtime = 1500
+    var milirandomshowtime = (1500..4000).random()
+    var difficulty = 0
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------------------------\\
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_game)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------\\
+
+    }
+    fun Startgamebutton(view: View) {
+        val timer2 = object: CountDownTimer(milirandomshowtime.toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val textbox = findViewById<TextView>(R. id. textviewnew)
+                textbox.text = milirandomshowtime.toString()
+            }
+
+            override fun onFinish() {
+                val timer = object: CountDownTimer(milishowtime.toLong(), 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        val textbox = findViewById<TextView>(R. id. textviewnew)
+                        textbox.text = kort.toString()
+                    }
+
+                    override fun onFinish() {
+                        val textbox = findViewById<TextView>(R. id. textviewnew)
+                        textbox.text = "Hidden"
+                    }
+                }
+                timer.start()
+                kort = (0..4).random()
+                milishowtime = milishowtime / 10
+                milishowtime = milishowtime * 9
+                difficulty++
+                milirandomshowtime = (1500..4000).random()
+
+            }
+        }
+        timer2.start()
     }
 }
