63c720c8484bc48ff58fb8b6e894b82c0660a58b
tuktukjohansson <marcusjohansson0111@gmail.com>
2021-09-23 16:10:48 +0200

    Lagt till bilder på kort som visar sig istället för text om vilket kort som ska visas, (text som visar kort nummer och delays finns dock kvar tills programmet blir färdigt för lättare felhantering tillsvidare) Lagt till knappar som ger rätt eller fel svar. Felhantering- Har flyttat på kod som körs varje gång "Ready up" knappen klickas på som ej bör köras i slutet av Funktionen Startgamebutton. Minskat upprepande kod. Sorterat kod i funktioner för lättare kod avläsning

--- app/src/main/java/com/example/mittreaktionspel/MainGameActivity.kt
+++ app/src/main/java/com/example/mittreaktionspel/MainGameActivity.kt
@@ -1,57 +1,115 @@
 package com.example.mittreaktionspel
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.properties.Delegates
 
 
-public class MainGameActivity : AppCompatActivity() {
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------------\\
-    //Variabler\\
-    var kort = (0..4).random()
+class MainGameActivity : AppCompatActivity() {
+    // Variabler \\
+    var kort = 0
     var milishowtime = 1500
-    var milirandomshowtime = (1500..4000).random()
+    var milirandomdelaytime = 0
     var difficulty = 0
+    var listofcards = listOf(
+        R.drawable.redheart, R.drawable.reddiamond,
+        R.drawable.blackclubs, R.drawable.blackspade)
+    // --------- \\
 
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------------\\
+    // On Create \\
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_game)
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------\\
-
     }
+    // --------- \\
+
+    // ------------------------------------Main Game------------------------------------ \\
     fun Startgamebutton(view: View) {
-        val timer2 = object: CountDownTimer(milirandomshowtime.toLong(), 1000) {
+        runcalcs()
+
+        val textbox = findViewById<TextView>(R.id.textviewnew)
+        val startgamebutton = findViewById<Button>(R.id.startgamebutton)
+        val Cardimage = findViewById<ImageView>(R.id.imagecard)
+
+        val timer2 = object : CountDownTimer(milirandomdelaytime.toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                val textbox = findViewById<TextView>(R. id. textviewnew)
-                textbox.text = milirandomshowtime.toString()
+                textbox.text = milirandomdelaytime.toString()
+                startgamebutton.visibility = View.GONE
             }
 
             override fun onFinish() {
-                val timer = object: CountDownTimer(milishowtime.toLong(), 1000) {
+
+                val timer = object : CountDownTimer(milishowtime.toLong(), 1000) {
+
                     override fun onTick(millisUntilFinished: Long) {
-                        val textbox = findViewById<TextView>(R. id. textviewnew)
                         textbox.text = kort.toString()
+                        Cardimage.setImageResource(listofcards[kort])
                     }
 
                     override fun onFinish() {
-                        val textbox = findViewById<TextView>(R. id. textviewnew)
                         textbox.text = "Hidden"
+                        startgamebutton.visibility = View.VISIBLE
+                        Cardimage.setImageResource(R.drawable.cards)
                     }
                 }
                 timer.start()
-                kort = (0..4).random()
-                milishowtime = milishowtime / 10
-                milishowtime = milishowtime * 9
-                difficulty++
-                milirandomshowtime = (1500..4000).random()
-
             }
         }
         timer2.start()
+
+    }
+    fun Answerheart(view: View) {
+        val textbox = findViewById<TextView>(R.id.textviewnew)
+        if (kort == 0){
+            textbox.setText("Rätt!")
+        }
+        else{
+            textbox.setText("Fel!")
+        }
+    }
+    fun Answerdiamond(view: View) {
+        val textbox = findViewById<TextView>(R.id.textviewnew)
+        if (kort == 1){
+            textbox.setText("Rätt!")
+        }
+        else{
+            textbox.setText("Fel!")
+        }
+    }
+    fun Answerclubs(view: View) {
+        val textbox = findViewById<TextView>(R.id.textviewnew)
+        if (kort == 2){
+            textbox.setText("Rätt!")
+        }
+        else{
+            textbox.setText("Fel!")
+        }
+    }
+    fun Answerspade(view: View) {
+        val textbox = findViewById<TextView>(R.id.textviewnew)
+        if (kort == 3){
+            textbox.setText("Rätt!")
+        }
+        else{
+            textbox.setText("Fel!")
+        }
+    }
+    // --------------------------------------------------------------------------------- \\
+    fun runcalcs() {
+        kort = (0..3).random()
+        milishowtime = milishowtime / 10
+        milishowtime = milishowtime * 9
+        difficulty++
+        milirandomdelaytime = (500..3500).random()
     }
 }
+
+
+
